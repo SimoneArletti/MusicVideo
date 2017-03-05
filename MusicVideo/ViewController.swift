0386959cc6855483367aa3da0ab8dbce4b2f3ce2
Simone Arletti <Simone@Mac-mini-di-Simone.local>
2017-03-05 23:14:53 +0100

    Initial Commit

---
+++ MusicVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVideo
+//
+//  Created by Simone Arletti on 05/03/17.
+//  Copyright © 2017 Simone Arletti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
